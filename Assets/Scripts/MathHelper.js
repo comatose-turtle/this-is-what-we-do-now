b18dc80fd23c93873733b506fe0870710e009468
SpitefulFox <diamondjbj2@yahoo.com>
2015-01-23 22:50:46 -0800

    Update

---
+++ Assets/Scripts/MathHelper.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+static function GetSign(num : float){
+	if(num > 0)
+		return 1;
+	else if(num < 0)
+		return -1;
+	else
+		return 0;
+}
---
+++ Assets/Scripts/WhatTheCharacterDoes.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var control : CharacterController;
+var speed : float = 30;
+
+
+function Awake () {
+	if(control == null)
+		control = gameObject.GetComponent(CharacterController);
+}
+
+function Update () {
+	var curSpeed : float;
+	curSpeed = speed;
+	var move : Vector3 = Vector3(Input.GetAxis("Horizontal") * curSpeed * Time.deltaTime, Input.GetAxis("Vertical") * curSpeed * Time.deltaTime, 0);
+	control.Move(move);
+}
