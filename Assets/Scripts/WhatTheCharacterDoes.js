b390145e077d9e67b4d0ea9f67ec8b620fa2887d
Thomas Orr <comatose_turtle@hotmail.com>
2015-01-24 15:18:27 -0800

    Rotate the World!

--- Assets/Scripts/WhatTheCharacterDoes.js
+++ Assets/Scripts/WhatTheCharacterDoes.js
@@ -4,8 +4,8 @@ var speed : float = 400;
 var speech : SpeechPopup;
 
 function FixedUpdate () {
-	var move : Vector3 = Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
-	rigidbody2D.velocity = move;
+	var move : Vector3 = Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+	rigidbody.velocity = move;
 	if(Input.GetButton("Fire1"))
 		speech.PopupText("A man, a plan, a canal, Panama.");
 }
