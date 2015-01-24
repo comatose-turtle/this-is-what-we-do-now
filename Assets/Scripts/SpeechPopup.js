3c4515763d3da7779bd1560c1056fd55e6d58eca
Thomas Orr <comatose_turtle@hotmail.com>
2015-01-24 00:08:18 -0800

    Speech bubbles

---
+++ Assets/Scripts/SpeechPopup.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+var displayText : UnityEngine.UI.Text;
+
+private var myText : String;
+private var popupTime : float = -1;
+private var myCanvasGroup : CanvasGroup;
+
+function Start () {
+	myCanvasGroup = transform.parent.GetComponent.<CanvasGroup>();
+	Invoke("Popit", 2);
+}
+
+function Update () {
+	if(popupTime > 0) {
+		if(Time.time - popupTime < 0.5)
+			myCanvasGroup.alpha = (Time.time - popupTime) * 2;
+		else if(Time.time - popupTime < 3)
+			myCanvasGroup.alpha = 1;
+		else if(Time.time - popupTime >= 3)
+			myCanvasGroup.alpha = Mathf.Clamp01(2 - (Time.time - (popupTime + 3))*2);
+	}
+	else
+		myCanvasGroup.alpha = 0;
+}
+
+function PopupText(str : String) {
+	popupTime = Time.time;
+	//myText = str;
+	displayText.text = str;
+}
+
+function Popit() {
+	PopupText("Here I am!");
+}
