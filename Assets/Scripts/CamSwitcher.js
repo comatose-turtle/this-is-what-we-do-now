9b15dc96e35c19df68d81ed80326f6b75d2f8c9a
Thomas Orr <comatose_turtle@hotmail.com>
2015-01-24 15:43:35 -0800

    Player back to 3D physics

--- Assets/Scripts/CamSwitcher.js
+++ Assets/Scripts/CamSwitcher.js
@@ -5,23 +5,38 @@ var char1 : WhatTheCharacterDoes;
 var camera2 : Camera;
 var char2 : WhatTheCharacterDoes;
 
-function Start () {
+private var audio1 : AudioListener;
+private var audio2 : AudioListener;
 
+function Start () {
+	audio1 = camera1.GetComponent.<AudioListener>();
+	audio2 = camera2.GetComponent.<AudioListener>();
+
+	ToggleCam2(false);
+	ToggleCam1(true);
 }
 
 function Update () {
 	if(Input.GetButtonDown("Fire2")) {
 		if(camera1.enabled) {
-			camera1.enabled = false;
-			char1.enabled = false;
-			camera2.enabled = true;
-			char2.enabled = true;
+			ToggleCam1(false);
+			ToggleCam2(true);
 		}
 		else {
-			camera1.enabled = true;
-			char1.enabled = true;
-			camera2.enabled = false;
-			char2.enabled = false;
+			ToggleCam2(false);
+			ToggleCam1(true);
 		}
 	}
+}
+
+private function ToggleCam1(yesno : boolean) {
+	camera1.enabled = yesno;
+	audio1.enabled = yesno;
+	char1.enabled = yesno;
+}
+
+private function ToggleCam2(yesno : boolean) {
+	camera2.enabled = yesno;
+	audio2.enabled = yesno;
+	char2.enabled = yesno;
 }
--- Assets/Scripts/WhatTheCharacterDoes.js
+++ Assets/Scripts/WhatTheCharacterDoes.js
@@ -2,10 +2,16 @@
 
 var speed : float = 400;
 var speech : SpeechPopup;
+var control : CharacterController;
 
-function FixedUpdate () {
+function Start () {
+	if(!control)
+		control = GetComponent.<CharacterController>();
+}
+
+function Update () {
 	var move : Vector3 = Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
-	rigidbody.velocity = move;
+	control.Move(move);
 	if(Input.GetButton("Fire1"))
 		speech.PopupText("A man, a plan, a canal, Panama.");
 }
