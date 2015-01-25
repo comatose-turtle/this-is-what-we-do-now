d1c9597a00c3ee0a8b02bd09cec9ef19e0abc90c
Paul Van Camp <mocha987-dev@yahoo.com>
2015-01-25 15:49:50 -0800

    Animating the switch when active

--- Assets/Scripts/FloorSwitch.js
+++ Assets/Scripts/FloorSwitch.js
@@ -1,7 +1,14 @@
 ﻿#pragma strict
+public var animate : boolean = false;
+public var rotSpeed : float = 200;
+public var scaleAmt : float = 4;
+public var scaleSpeed : float = 100;
+
+private var _scaleTm : float = 0;
 
 function OnTriggerEnter(other : Collider){
 	if(other.gameObject.CompareTag("Player")){
+		animate= true;
 		if(GameMaster.instance != null)
 			GameMaster.instance.SendMessage("CollisionDecision", [other.gameObject, gameObject]);
 	}
@@ -9,7 +16,30 @@ function OnTriggerEnter(other : Collider){
 
 function OnTriggerExit(other : Collider){
 	if(other.gameObject.CompareTag("Player")){
+		animate= false;
+		
 		if(GameMaster.instance != null)
 			GameMaster.instance.SendMessage("UncollisionDecision", [other.gameObject, gameObject]);
+	}
+}
+
+function Update () {
+	if ( animate ) {
+		//rotate the object
+		var rot= gameObject.transform.rotation.eulerAngles;
+		rot.y += rotSpeed * Time.deltaTime;
+		gameObject.transform.rotation.eulerAngles= rot;
+		
+		//Scale the size
+		if ( _scaleTm == 0 ) _scaleTm= Time.time;
+		var sc= Mathf.Sin( (Time.time - _scaleTm ) * scaleSpeed );
+		gameObject.transform.localScale.x= sc;
+		gameObject.transform.localScale.z= sc;
+	}
+	else if ( _scaleTm != 0 ) {
+		//reset
+		_scaleTm= 0;
+		gameObject.transform.localScale.x= 1;
+		gameObject.transform.localScale.z= 1;
 	}
 }
