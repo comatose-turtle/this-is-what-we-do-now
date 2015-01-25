00703a6a184a4665ddace58e6492c10557490214
SpitefulFox <diamondjbj2@yahoo.com>
2015-01-24 20:30:08 -0800

    Smoother Sticky

--- Assets/Scripts/StickyCam.js
+++ Assets/Scripts/StickyCam.js
@@ -9,8 +9,8 @@ function Start () {
 	relative = transform.position - target.position;
 }
 
-function FixedUpdate () {
-	if(Vector3.Distance(transform.position, target.position) > followDistance){
-		transform.position = Vector3.MoveTowards(transform.position, target.position + relative, Vector3.Distance(transform.position, target.position) - followDistance);
-	}
+function Update () {
+	//if(Vector3.Distance(transform.position, target.position + relative) > followDistance){
+		transform.position = Vector3.MoveTowards(transform.position, target.position + relative, Mathf.Max(Vector3.Distance(transform.position, target.position) - followDistance, 0));
+	//}
 }
