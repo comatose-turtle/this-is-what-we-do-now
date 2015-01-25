a49df1b5a8de2b9bf6720d0b218e5ce6393bc019
Paul Van Camp <mocha987-dev@yahoo.com>
2015-01-25 13:24:23 -0800

    Added screen credits to WinScene and left speach bubble.

---
+++ Assets/Scripts/CreditScroller.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var _heightStart : float = 1;
+var _heightEnd : float = 10;
+var _scrollTime : float = 40;
+var _startDelay : float = 10;
+
+private var _startTm : float;
+
+function Start () {
+	_startTm= Time.time;
+}
+
+function Update () {
+	var tmDelta= Time.time - _startTm - _startDelay;
+	var ht= Mathf.Lerp( _heightStart, _heightEnd, tmDelta/_scrollTime );
+	camera.transform.position.z= ht;
+}
